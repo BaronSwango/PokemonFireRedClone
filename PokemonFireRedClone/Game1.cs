91259b50f035ee47fa46f017a79d456860a6eb06
BaronSwango <baronswango30@gmail.com>
2021-12-11 22:08:34 -0500

    ScreenManager

--- PokemonFireRedClone/Game1.cs
+++ PokemonFireRedClone/Game1.cs
@@ -6,21 +6,21 @@ namespace PokemonFireRedClone
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
+        private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-
-        private Texture2D spriteSheet;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = (int)ScreenManager.Instance.Dimensions.X;
+            graphics.PreferredBackBufferHeight = (int)ScreenManager.Instance.Dimensions.Y;
+            graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -29,8 +29,10 @@ namespace PokemonFireRedClone
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            ScreenManager.Instance.LoadContent(Content);
             // TODO: use this.Content to load your game content here
-            spriteSheet = Content.Load<Texture2D>("red");
+
+
          }
 
         protected override void Update(GameTime gameTime)
@@ -38,6 +40,7 @@ namespace PokemonFireRedClone
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            ScreenManager.Instance.Update(gameTime);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -47,12 +50,8 @@ namespace PokemonFireRedClone
         {
             GraphicsDevice.Clear(Color.White);
 
-            // TODO: Add your drawing code here
-            spriteBatch.Begin();
-
-            spriteBatch.Draw(spriteSheet, new Vector2(100, 100), Color.White);
-
-            spriteBatch.End();
+            ScreenManager.Instance.Draw(spriteBatch);
+  
 
             base.Draw(gameTime);
         }
---
+++ PokemonFireRedClone/ScreenManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PokemonFireRedClone
+{
+    public class ScreenManager
+    {
+
+
+        private static ScreenManager instance;
+        public Vector2 Dimensions { private set; get; }
+        public static ScreenManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ScreenManager();
+
+                return instance;
+            }
+        }
+
+        public ScreenManager()
+        {
+            Dimensions = new Vector2(640, 480);
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+
+        }
+
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+
+    }
+}
