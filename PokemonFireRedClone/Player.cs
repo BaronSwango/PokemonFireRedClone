b85a9d9f97f70d1fd98f9f89be6d9b60d120f0df
BaronSwango <95740113+BaronSwango@users.noreply.github.com>
2021-12-20 17:20:53 -0500

    Cleaned up the tile based movement (basically complete)

--- PokemonFireRedClone/Player.cs
+++ PokemonFireRedClone/Player.cs
@@ -16,7 +16,10 @@ namespace PokemonFireRedClone
 
         public float MoveSpeed;
         Vector2 destination;
+        int waitToMove;
+        bool changeDirection;
         public State state;
+        State prevState;
 
         public enum State { Idle, MoveRight, MoveLeft, MoveUp, MoveDown }
 
@@ -25,12 +28,13 @@ namespace PokemonFireRedClone
             Velocity = Vector2.Zero;
             destination = Vector2.Zero;
             state = State.Idle;
+            changeDirection = false;
+            waitToMove = 0;
         }
 
         public void LoadContent()
         {
             Image.LoadContent();
-            destination = Image.Position;
         }
 
         public void UnloadContent()
@@ -47,37 +51,73 @@ namespace PokemonFireRedClone
                 case State.Idle:
                     destination = Image.Position;
 
+                    // causes a change in direction but no movement unless key is held down more than 4 iterations of the Update method
+                    if (changeDirection && prevState == State.Idle)
+                    {
+                        if (waitToMove < 4)
+                        {
+                            if (!InputManager.Instance.KeyDown(Keys.W, Keys.A, Keys.S, Keys.D))
+                                state = State.Idle;
+                            waitToMove++;
+                            break;
+                        }
+                        changeDirection = false;
+                        waitToMove = 0;
+                    }
 
                     if (InputManager.Instance.KeyDown(Keys.W))
                     {
+                        if (Image.SpriteSheetEffect.CurrentFrame.Y != 3)
+                        {
+                            Image.SpriteSheetEffect.CurrentFrame.Y = 3;
+                            changeDirection = true;
+                            break;
+                        }
                         destination.Y -= 64;
                         state = State.MoveUp;
-                        Image.SpriteSheetEffect.CurrentFrame.Y = 3;
 
                     }
                     else if (InputManager.Instance.KeyDown(Keys.S))
                     {
+                        if (Image.SpriteSheetEffect.CurrentFrame.Y != 2)
+                        {
+                            Image.SpriteSheetEffect.CurrentFrame.Y = 2;
+                            changeDirection = true;
+                            break;
+                        }
                         destination.Y += 64;
                         state = State.MoveDown;
-                        Image.SpriteSheetEffect.CurrentFrame.Y = 2;
                     }
                     else if (InputManager.Instance.KeyDown(Keys.A))
                     {
+                        if (Image.SpriteSheetEffect.CurrentFrame.Y != 0)
+                        {
+                            Image.SpriteSheetEffect.CurrentFrame.Y = 0;
+                            changeDirection = true;
+                            break;
+                        }
                         destination.X -= 64;
                         state = State.MoveLeft;
-                        Image.SpriteSheetEffect.CurrentFrame.Y = 0;
                     }
                     else if (InputManager.Instance.KeyDown(Keys.D))
                     {
+                        if (Image.SpriteSheetEffect.CurrentFrame.Y != 1)
+                        {
+                            Image.SpriteSheetEffect.CurrentFrame.Y = 1;
+                            changeDirection = true;
+                            break;
+                        }
                         destination.X += 64;
                         state = State.MoveRight;
-                        Image.SpriteSheetEffect.CurrentFrame.Y = 1;
                     } else
                         Image.IsActive = false;
 
-
+                    prevState = State.Idle;
                     break;
                 case State.MoveUp:
+
+                    // causes a change in direction but no movement unless key is held down more than 4 iterations of the Update method
+
 
                     if (Image.Position.Y - (int) (MoveSpeed * (float)gameTime.ElapsedGameTime.TotalMilliseconds) < (int) destination.Y)
                     {
@@ -86,30 +126,41 @@ namespace PokemonFireRedClone
                         destination.Y -= 64;
 
                         if (!InputManager.Instance.KeyDown(Keys.W))
+                        {
                             state = State.Idle;
+                            prevState = State.MoveUp;
+                        }
                     }
                     else
                         Image.Position.Y -= (int)(MoveSpeed * (float)gameTime.ElapsedGameTime.TotalMilliseconds);
                     break;
                 case State.MoveDown:
+
                     if (Image.Position.Y + (int) (MoveSpeed * (float)gameTime.ElapsedGameTime.TotalMilliseconds) > (int) destination.Y)
                     {
                         Image.Position.Y = (int) destination.Y;
                         destination.Y += 64;
 
                         if (!InputManager.Instance.KeyDown(Keys.S))
+                        {
                             state = State.Idle;
+                            prevState = State.MoveDown;
+                        }
                     }
                     else
                         Image.Position.Y += (int) (MoveSpeed * (float)gameTime.ElapsedGameTime.TotalMilliseconds);
                     break;
                 case State.MoveLeft:
+
                     if (Image.Position.X - MoveSpeed * (float)gameTime.ElapsedGameTime.TotalMilliseconds < destination.X)
                     {
                         Image.Position.X = (int) destination.X;
                         destination.X -= 64;
                         if (!InputManager.Instance.KeyDown(Keys.A))
+                        {
                             state = State.Idle;
+                            prevState = State.MoveLeft;
+                        }
                     }
                     else
                         Image.Position.X -= (int) (MoveSpeed * (float)gameTime.ElapsedGameTime.TotalMilliseconds);
@@ -121,7 +172,10 @@ namespace PokemonFireRedClone
                         Image.Position.X = (int) destination.X;
                         destination.X += 64;
                         if (!InputManager.Instance.KeyDown(Keys.D))
+                        {
                             state = State.Idle;
+                            prevState = State.MoveRight;
+                        }
                     }
                     else
                         Image.Position.X += (int) (MoveSpeed * (float)gameTime.ElapsedGameTime.TotalMilliseconds);
